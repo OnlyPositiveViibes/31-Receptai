16b883cf5a3968b127798d1ee7a61c01a1f3a4c4
Edvinas Ramanauskas <edvinyzas.rr@gmail.com>
2021-10-28 19:51:46 +0300

    html converting  into js

---
+++ pages/home.js
@@ -0,0 +1,47 @@
+function pageHome() {
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Receptai</title>
+    
+        <base href="http://localhost:3000/home/">
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+        <link rel="manifest" href="./site.webmanifest">
+        <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+    
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/components/button.css">
+    </head>
+    
+    <body>
+        <h1>Receptai</h1>
+        <p>Cia bus patys blogiausi receptai pasaulyje - uzsukite kai bus paruosta 😈🍕</p>
+        <div class="btn">Click me!</div>
+        <div>
+    
+            <p><a href="/pages/404.html">404</a></p>
+            <p><a href="http://localhost:3000/about">About</a></p>
+            <p><a href="http://localhost:3000/contact">Contact</a></p>
+            <p><a href="http://localhost:3000/paslaugos">Services</a></p>
+            <p><a href="http://localhost:3000/receptait">Receips</a></p>
+        </div>
+        <img src="./img/saliami-1.jpg" alt="Saliamis">
+        <img src="./img/kebab.jpg" alt="Vaikiskas kebabas">
+        <script src="./js/main.js"></script>
+    </body>
+    
+    </html>`;
+}
+
+module.exports = pageHome;
